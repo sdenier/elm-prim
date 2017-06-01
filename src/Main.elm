13cb3339c3f28904cddd1071e61ab60eedc9a85e
Simon Denier <simon@sogilis.com>
2017-06-01 14:31:02 +0200

    Define Prim algorithm - working but need refactoring
    No Json output yet

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,12 @@
 module Main exposing (..)
 
+import Dict exposing (Dict)
 import Html exposing (Html, button, div, input, text, textarea)
 import Html.Attributes exposing (style, value)
 import Html.Events exposing (onClick, onInput)
 import Random
+import Random.Set as Set
+import Set exposing (Set)
 
 
 main : Program Never Model Msg
@@ -18,13 +21,23 @@ main =
 
 type alias Model =
     { size : Int
+    , toVisit : Set Square
+    , visited : DictWalls
     , output : String
     }
 
 
+type alias DictWalls =
+    Dict Square Walls
+
+
+type alias Walls =
+    List String
+
+
 init : ( Model, Cmd Msg )
 init =
-    ( { size = 3, output = "" }, Cmd.none )
+    ( { size = 3, toVisit = Set.empty, visited = Dict.empty, output = "" }, Cmd.none )
 
 
 type alias Square =
@@ -40,13 +53,49 @@ runPrim mazeSize =
         squareGen =
             Random.pair (Random.int 0 upperBound) (Random.int 0 upperBound)
     in
-        Random.generate FirstSquare squareGen
+        Random.generate PrimInit squareGen
+
+
+findNeighbors : Int -> Square -> List Square
+findNeighbors maxSize square =
+    let
+        ( x, y ) =
+            square
+
+        neighbors =
+            [ ( x - 1, y ), ( x + 1, y ), ( x, y - 1 ), ( x, y + 1 ) ]
+    in
+        List.filter (\( x, y ) -> x >= 0 && x < maxSize && y >= 0 && y < maxSize) neighbors
+
+
+openWall : Square -> Square -> DictWalls -> Walls
+openWall source target dictWalls =
+    let
+        sourceWalls =
+            Dict.get source dictWalls |> Maybe.withDefault []
+
+        ( sourceX, sourceY ) =
+            source
+
+        ( targetX, targetY ) =
+            target
+    in
+        if sourceX > targetX then
+            "W" :: sourceWalls
+        else if sourceX < targetX then
+            "E" :: sourceWalls
+        else if sourceY > targetY then
+            "N" :: sourceWalls
+        else
+            "S" :: sourceWalls
 
 
 type Msg
     = ChangeSize String
-    | FirstSquare Square
     | GenMaze
+    | PrimInit Square
+    | PrimNextVisit (Maybe Square)
+    | PrimOpenWall Square (Maybe Square)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -59,12 +108,91 @@ update msg model =
             in
                 ( { model | size = newSize }, Cmd.none )
 
-        FirstSquare s ->
+        PrimInit square ->
             let
                 _ =
-                    Debug.log ("toto" ++ toString s)
+                    Debug.log "PrimInit" (toString square)
+
+                visited =
+                    Dict.singleton square []
+
+                toVisit =
+                    Set.fromList (findNeighbors model.size square)
+
+                newModel =
+                    { model | toVisit = toVisit, visited = visited, output = (toString square) }
+
+                nextStep =
+                    Random.generate PrimNextVisit (Set.sample toVisit)
             in
-                ( { model | output = (toString s) }, Cmd.none )
+                ( newModel, nextStep )
+
+        PrimNextVisit (Just square) ->
+            let
+                _ =
+                    Debug.log "PrimNextVisit" (toString square ++ " " ++ (toString visitedNeighbors) ++ " " ++ (toString toVisitNeighbors))
+
+                visited =
+                    Dict.insert square [] model.visited
+
+                neighbors =
+                    Set.fromList (findNeighbors model.size square)
+
+                ( visitedNeighbors, toVisitNeighbors ) =
+                    Set.partition (\s -> Dict.member s model.visited) neighbors
+
+                toVisit =
+                    Set.union (Set.remove square model.toVisit) toVisitNeighbors
+
+                newModel =
+                    { model | toVisit = toVisit, visited = visited, output = (toString square) }
+
+                nextStep =
+                    Random.generate (PrimOpenWall square) (Set.sample visitedNeighbors)
+            in
+                ( newModel, nextStep )
+
+        PrimNextVisit Nothing ->
+            let
+                _ =
+                    Debug.log "Next" "Done"
+            in
+                ( model, Cmd.none )
+
+        PrimOpenWall sourceSquare (Just targetSquare) ->
+            let
+                _ =
+                    Debug.log "PrimOpenWall" (toString sourceSquare ++ " -> " ++ toString targetSquare)
+
+                sourceWalls =
+                    openWall sourceSquare targetSquare model.visited
+
+                targetWalls =
+                    openWall targetSquare sourceSquare model.visited
+
+                dictUpdate =
+                    Dict.fromList [ ( sourceSquare, sourceWalls ), ( targetSquare, targetWalls ) ]
+
+                visited =
+                    Dict.union dictUpdate model.visited
+
+                newModel =
+                    { model | visited = visited }
+
+                nextStep =
+                    Random.generate PrimNextVisit (Set.sample model.toVisit)
+
+                _ =
+                    Debug.log "PrimOpenWall" visited
+            in
+                ( newModel, nextStep )
+
+        PrimOpenWall _ Nothing ->
+            let
+                _ =
+                    Debug.log "PrimOpenWall" "Something is wrong"
+            in
+                ( model, Cmd.none )
 
         GenMaze ->
             ( model, runPrim model.size )
