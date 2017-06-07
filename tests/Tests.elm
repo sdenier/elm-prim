a60d5126af451fb0eef85fbfe035c206d84f3b66
Simon Denier <simon@sogilis.com>
2017-06-07 14:07:48 +0200

    Refactor tests

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -10,35 +10,45 @@ import Set exposing (Set)
 all : Test
 all =
     describe "Maze Generation with Prim's Algorithm"
-        [ describe "squareIdString"
-            [ test "transforms square coordinates into an id string" <|
-                \() ->
-                    Expect.equal "3,7" (squareIdString ( 3, 7 ))
-            , fuzz2 int int "concatenates both coordinates to create the id" <|
-                \x y ->
-                    (squareIdString ( x, y )) |> Expect.equal ((toString x) ++ "," ++ (toString y))
-            ]
-        , describe "findNeighbors"
-            [ test "gives the 4 neighbours around the give square" <|
-                \() ->
-                    let
-                        expected =
-                            Set.fromList [ ( 1, 1 ), ( 3, 1 ), ( 2, 0 ), ( 2, 2 ) ]
-                    in
-                        (findNeighbors 4 ( 2, 1 )) |> Expect.equal expected
-            , test "excludes neighbours out of maze bounds" <|
-                \() ->
-                    let
-                        expected =
-                            Set.fromList [ ( 1, 0 ), ( 0, 1 ) ]
-                    in
-                        (findNeighbors 4 ( 0, 0 )) |> Expect.equal expected
-            , test "excludes neighbours out of maze bounds" <|
-                \() ->
-                    let
-                        expected =
-                            Set.fromList [ ( 2, 3 ), ( 3, 2 ) ]
-                    in
-                        (findNeighbors 4 ( 3, 3 )) |> Expect.equal expected
-            ]
+        [ test_squareIdString
+        , test_findNeighbors
+        ]
+
+
+test_squareIdString : Test
+test_squareIdString =
+    describe "squareIdString"
+        [ test "transforms square coordinates into an id string" <|
+            \() ->
+                Expect.equal "3,7" (squareIdString ( 3, 7 ))
+        , fuzz2 int int "concatenates both coordinates to create the id" <|
+            \x y ->
+                (squareIdString ( x, y )) |> Expect.equal ((toString x) ++ "," ++ (toString y))
+        ]
+
+
+test_findNeighbors : Test
+test_findNeighbors =
+    describe "findNeighbors"
+        [ test "gives the 4 neighbours around the give square" <|
+            \() ->
+                let
+                    expected =
+                        Set.fromList [ ( 1, 1 ), ( 3, 1 ), ( 2, 0 ), ( 2, 2 ) ]
+                in
+                    (findNeighbors 4 ( 2, 1 )) |> Expect.equal expected
+        , test "excludes neighbours out of maze bounds" <|
+            \() ->
+                let
+                    expected =
+                        Set.fromList [ ( 1, 0 ), ( 0, 1 ) ]
+                in
+                    (findNeighbors 4 ( 0, 0 )) |> Expect.equal expected
+        , test "excludes neighbours out of maze bounds" <|
+            \() ->
+                let
+                    expected =
+                        Set.fromList [ ( 2, 3 ), ( 3, 2 ) ]
+                in
+                    (findNeighbors 4 ( 3, 3 )) |> Expect.equal expected
         ]
